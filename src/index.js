f963e5de4283b742d3fb1a9bbcbca1a204d75590
Muhammad Arafat <3rafat@gmail.com>
2018-12-26 12:05:44 -0500

    Add mongoose connection. Rearrange app startup code.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-const fastify = require('fastify')({
-    logger: true
-});
+const fastify = require('fastify')({ logger: true });
+const mongoose = require('mongoose');
+
+// connect to db
+const connectDB = async (fastify, mongoose) => {
+    try {
+        await mongoose.connect('mongodb://localhost/mycargarage', { useNewUrlParser: true });
+        fastify.log.info('MongoDB connected...');
+    } catch (e) {
+        fastify.log.error(e);
+    }
+};
 
 fastify.get('/', async (request, reply) => {
     return { hello: 'world' }
 });
 
-const start = async () => {
+const startServer = async (fastify) => {
     try {
         await fastify.listen(3210);
     } catch (e) {
@@ -15,4 +24,7 @@ const start = async () => {
     }
 };
 
-start();
+(async function (fastify, mongoose){
+  await connectDB(fastify, mongoose);
+  await startServer(fastify);
+})(fastify, mongoose);
